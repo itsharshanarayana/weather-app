10a27d90d108ed86675dbe9ecfe54e1767d06355
harsha <harsha@localhost.localdomain>
2017-09-13 21:05:05 -0400

    Getting the formatted address

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4%20Royal%20Crest%20dr,%20nashua,%20nh',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, null, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
 });
