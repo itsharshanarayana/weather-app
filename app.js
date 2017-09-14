8bebb7666ed0df3d2f6e1edc15ce8af0e3cc47dd
harsha <harsha@localhost.localdomain>
2017-09-13 21:15:34 -0400

    Getting location/lat/long for the location

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ request({
   // console.log(JSON.stringify(body, undefined, 2));
   // Using template strings, print formatted address.
   console.log(`Address: ${body.results[0].formatted_address}`);
+  // Getting location object which contains lattitude and longitude of the
+  // location.
+  var location = JSON.stringify(body.results[0].geometry.location, undefined, 2);
+  console.log(`Location: ${location}`);
+  // Getting latitude for the location.
   console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+  // Getting longitude for the location.
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
