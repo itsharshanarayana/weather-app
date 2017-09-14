7c39a4c12614c822cbcef9ac71278fa81d6a068f
harsha <harsha@localhost.localdomain>
2017-09-13 20:50:59 -0400

    Pretty printing the body object

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4%20Royal%20Crest%20dr,%20nashua,%20nh',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, null, 2));
 });
