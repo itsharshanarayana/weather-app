d24997ac791a4f5b10f0635038ee8759945f5357
harsha <harsha@localhost.localdomain>
2017-09-16 17:19:15 -0400

    gecode module added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const request = require('request');
+// Import third part modules
 const yargs = require('yargs');
 
+// Import custom modules
+const geocode = require('./geocode/geocode.js');
+
+// Variables/Constant declarations
 const argv = yargs
   .options({
     a: {
@@ -14,53 +18,12 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// console.log('Actual yargs.argv' + argv);
-// Getting address value passed in command prompt
-console.log(`Address: ${yargs.argv.a}`);
 
-// Encode command line argument - address
-var encodedAddress = encodeURIComponent(yargs.argv.a);
-// console.log('Encoded Address: ' + encodedAddress);
+// var location = geocode.geocodeAddress(yargs.argv.a);
 
-// Decoded command line argument - address
-var decodedAddress = decodeURIComponent(encodedAddress);
-// console.log('Decoded Address: ' + decodedAddress);
+geocode.geocodeAddress(yargs.argv.a);
 
-// Get the complete URL
-var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-var url = baseURL + encodedAddress;
 
-console.log('URL: ' + url);
-
-request({
-  // url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4%20Royal%20Crest%20dr,%20nashua,%20nh',
-  url,
-  json: true
-
-  // console.log(url);
-}, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  // Using template strings, print formatted address.
-  // console.log(`Address: ${body.results[0].formatted_address}`);
-  // Getting location object which contains lattitude and longitude of the
-  // location.
-  // var location = JSON.stringify(body.results[0].geometry.location, undefined, 2);
-  // console.log(`Location: ${location}`);
-  // Getting latitude for the location.
-  if(error){
-    console.log('Unable to connect to Google Geocode API servers');
-  } else if('ZERO_RESULTS' === body.status){
-    console.log('Invalid address. Please correct the address');
-  } else if('OK' === body.status){
-    console.log('\nAddress details:');
-    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-    // Getting longitude for the location.
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-
-    // console.log(`Response Status Code: ${response.statusCode}`);
-    // console.log('Status Code: ' + response.statusCode);
-    // console.log(JSON.stringify(response.request.uri.href,undefined, 2));
-    console.log('Response status: ' + body.status);
-  }
-
-});
+// console.log('Location Object Returned: ' + JSON.stringify(location, undefined, 2));
+// console.log('Latitude: ' + location.lat);
+// console.log('Longitude: ' + location.lng);
---
+++ geocode/geocode.js
@@ -0,0 +1,43 @@
+// Import third party modules
+const request = require('request');
+
+// Import custom modules
+
+// Variable/Constants declarations
+const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+
+
+var geocodeAddress = (raw_address) => {
+  console.log('Raw Address: '+ raw_address);
+  const url = baseURL + encodeURIComponent(raw_address);
+  console.log('Complete URI: ' + url);
+
+  // Result to be passed to the calling routine
+  var my_result = {};
+  
+  request({
+    url,
+    json: true
+  }, (error, response, body) => {
+
+    if(error){
+      console.log('Unable to connect to Google Geocode API servers');
+    } else if('ZERO_RESULTS' === body.status){
+      console.log('Invalid address. Please correct the address');
+    } else if('OK' === body.status){
+      my_result.complete_address = body.results[0].formatted_address;
+      my_result.latitude = body.results[0].geometry.location.lat;
+      my_result.longitude = body.results[0].geometry.location.lng;
+
+      console.log('Result Object: ' + JSON.stringify(my_result, undefined, 2));
+    }
+
+  });
+
+}; // End of geocodeAddress
+
+// Expose function geocodeAddress so that it is visible to the calling modules
+module.exports = {
+  geocodeAddress
+};
